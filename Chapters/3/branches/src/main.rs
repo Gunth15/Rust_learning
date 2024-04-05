d688a290ec4e7710c3f9377944ac09c4c240ea59
Gunth15 <113713364+Gunth15@users.noreply.github.com>
2024-04-05 11:42:24 -0500

    Update main.rs

--- Chapters/3/branches/src/main.rs
+++ Chapters/3/branches/src/main.rs
@@ -11,6 +11,6 @@ fn main() {
     let condition = true;
     
     //if statement is an expression, so it can be used as follows, variable will be bound based on
-    //outcome this also means each ar must have the same type
+    //outcome this also means each var must have the same type
     let x = if condition {5} else {6};
 }
