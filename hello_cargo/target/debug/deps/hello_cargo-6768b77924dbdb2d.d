752b95b9fb24b1a0db2461b440094d266db23adc
Gunth15 <113713364+Gunth15@users.noreply.github.com>
2024-04-05 11:52:44 -0500

    Hello world

---
+++ hello_cargo/target/debug/deps/hello_cargo-6768b77924dbdb2d.d
@@ -0,0 +1,5 @@
+/Users/cw/Documents/rustic/projects/hello_cargo/target/debug/deps/hello_cargo-6768b77924dbdb2d: src/main.rs
+
+/Users/cw/Documents/rustic/projects/hello_cargo/target/debug/deps/hello_cargo-6768b77924dbdb2d.d: src/main.rs
+
+src/main.rs:
