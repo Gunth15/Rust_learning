2f832bcb5322aee07803c12f208f71a44693a5da
Gunth15 <113713364+Gunth15@users.noreply.github.com>
2024-04-05 11:55:30 -0500

    Fibbonacci sequence in rust

---
+++ fib/target/debug/deps/fib-e8375fdf8432fcca.d
@@ -0,0 +1,5 @@
+/Users/cw/Documents/rustic/projects/fib/target/debug/deps/fib-e8375fdf8432fcca: src/main.rs
+
+/Users/cw/Documents/rustic/projects/fib/target/debug/deps/fib-e8375fdf8432fcca.d: src/main.rs
+
+src/main.rs:
---
+++ fib/target/debug/fib.d
@@ -0,0 +1,1 @@
+/Users/cw/Documents/rustic/projects/fib/target/debug/fib: /Users/cw/Documents/rustic/projects/fib/src/main.rs
